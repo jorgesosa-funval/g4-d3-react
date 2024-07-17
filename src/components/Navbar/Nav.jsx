b027f1e8a9f91659c19fccc52ec9ca62795d3454
Mauro Agostinelli <maufabagosgam@gmail.com>
2024-07-17 11:28:41 -0500

    actualizacion de boton Nav

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import equis from './icons/equis.png';
+import menu from './icons/menu.png'
 
 export const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,7 +11,7 @@ export const Nav = () => {
 
   return (
     <div className='font-PrincipalFont'>
-      <button onClick={toggleNav} className='p-2 ml-5 mt-5 bg-[#2f2f31] text-white rounded-lg hover:bg-[#85868b] absolute top-0'>Open Nav</button>
+      <button onClick={toggleNav} className='p-2 ml-5 mt-5 bg-[#2f2f31] text-white rounded-lg hover:bg-[#85868b] absolute top-0'><img src={menu} alt="menu icon" /></button>
       <div className={`fixed top-0 left-0 h-screen bg-[#2f2f31] w-[20%] flex flex-col rounded-r-xl transform transition-transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
         <button onClick={toggleNav} className='flex content-center w-full border-black border-2px '>
           <img src={equis} alt="icono equis" className='mx-2 my-2 w-7 h-7' /> <p className='relative top-3 uppercase underline text-center left-0 right-0 w-[70%] text-zinc-500'>Menú:</p>
