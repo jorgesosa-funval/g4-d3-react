b2746b222f0f0e3b0302d572aabd89e5b7514a7e
Adrian Saltos <Asaltos>
2024-07-17 12:38:10 -0500

    Actualizacion de Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import ProductPage from './pages/ProductPage'
 
 export default function App() {
   return (
-    <>
-      <ProductPage />
-    </>
+    <div>App</div>
   )
 }
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Product } from '../components/Product/Product'
 import { Footer } from '../components/Product/Footer'
+import Layout from '../components/Product/Layout'
 
 export default function Home() {
   return (
     <>
+      <Layout />
       <Product />
       <Footer />
     </>
