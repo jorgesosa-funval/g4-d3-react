25871b54af14f7bea2c72c2ba85567074581fb58
Mauro Agostinelli <maufabagosgam@gmail.com>
2024-07-18 22:28:18 -0500

    nuevo nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Navbar } from './components/Nav2/Navbar'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
