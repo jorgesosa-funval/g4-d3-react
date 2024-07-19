2ce72e33a56e2eea800ff5a009f233dd822a1428
Mauro Agostinelli <maufabagosgam@gmail.com>
2024-07-18 22:31:06 -0500

    limpieza

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Navbar } from './components/Nav2/Navbar'
 
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
+      
     </div>
   )
 }
